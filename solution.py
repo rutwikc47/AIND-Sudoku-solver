51c1d661e0257ac3499bf37aad285f4b4007d417
Rutwik <rutwikc47@gmail.com>
2017-02-19 18:27:51 +0530

    Implemented both functions

---
+++ solution.py
@@ -0,0 +1,190 @@
+assignments = []
+
+rows = 'ABCDEFGHI'
+cols = '123456789'
+cols2='987654321'
+
+def cross(A, B):
+    "Cross product of elements in A and elements in B."
+    return [s+t for s in A for t in B]
+
+    pass
+
+boxes = cross(rows, cols)
+
+row_units = [cross(r, cols) for r in rows]
+column_units = [cross(rows, c) for c in cols]
+square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+unitlist = row_units + column_units + square_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
+
+#Get the Diagonals
+
+diag1=list(rows[i]+cols[i] for i in range(len(rows)))
+diag2=list(rows[i]+cols2[i] for i in range(len(rows)))
+
+def assign_value(values, box, value):
+
+    values[box] = value
+    if len(value) == 1:
+        assignments.append(values.copy())
+    return values
+
+def grid_values(grid):
+
+    chars = []
+    digits = '123456789'
+    for c in grid:
+        if c in digits:
+            chars.append(c)
+        if c == '.':
+            chars.append(digits)
+    assert len(chars) == 81
+    return dict(zip(boxes, chars))
+    pass
+
+def display(values):
+
+    width = 1+max(len(values[s]) for s in boxes)
+    line = '+'.join(['-'*(width*3)]*3)
+    for r in rows:
+        print(''.join(values[r+c].center(width)+('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF': print(line)
+    print
+
+def eliminate(values):
+
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    for box in solved_values:
+        digit = values[box]
+        for peer in peers[box]:
+            values[peer] = values[peer].replace(digit, '')
+
+    """
+
+    Diagonal Sudoku eliminator:-
+
+    """
+
+    solved_diag1=[diag for diag in diag1 if len(values[diag]) == 1]
+    solved_diag2=[diag for diag in diag2 if len(values[diag]) == 1]
+
+    if solved_diag1:
+        for diag in solved_diag1:
+            digit=values[diag]
+            for diag in diag1:
+                if len(values[diag])>=2:
+                    values[diag]=values[diag].replace(digit,'')
+    if solved_diag2:
+        for diag in solved_diag2:
+            digit=values[diag]
+            for diag in diag2:
+                if len(values[diag])>=2:
+                    values[diag]=values[diag].replace(digit,'')
+
+    return values
+
+def only_choice(values):
+
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
+
+def reduce_puzzle(values):
+    solved_values = [box for box in values.keys() if len(values[box]) == 1]
+    stalled = False
+    while not stalled:
+        solved_values_before = len([box for box in values.keys() if len(values[box]) == 1])
+        values = eliminate(values)
+        values = only_choice(values)
+        values=naked_twins(values)
+        solved_values_after = len([box for box in values.keys() if len(values[box]) == 1])
+        stalled = solved_values_before == solved_values_after
+        if len([box for box in values.keys() if len(values[box]) == 0]):
+            return False
+    return values
+
+def search(values):
+
+    values = reduce_puzzle(values)
+    if values is False:
+        return False  ## Failed earlier
+    if all(len(values[s]) == 1 for s in boxes):
+        return values  ## Solved!
+    # Choose one of the unfilled squares with the fewest possibilities
+    n, s = min((len(values[s]), s) for s in boxes if len(values[s]) > 1)
+    # Now use recurrence to solve each one of the resulting sudokus, and
+    for value in values[s]:
+        new_sudoku = values.copy()
+        new_sudoku[s] = value
+        attempt = search(new_sudoku)
+        if attempt:
+            return attempt
+
+def naked_twins(values):
+
+    """
+
+    Naked Twins:-
+
+    """
+
+    #Get the Naked twins in the sameval list
+
+    sameval = list()
+    for peer, val in peers.items():
+        for v in val:
+            if values[peer] == values[v] and len(values[peer]) == 2:
+                temp = list((peer, v))
+                sameval.append(temp)
+
+    diglist = list(x[0] for x in sameval)
+
+    #Get the Peers for the Pair of Naked Twins in the sameval list
+
+    peerlist = list(set(peers[val[0]]) & set(peers[val[1]]) for val in sameval)
+
+    #Remove the Naked twin's value from their peers
+
+    if sameval:
+        count = 0
+        for ele in peerlist:
+            digit = values[diglist[count]]
+            for e in ele:
+                if len(values[e]) > 2:
+                    values[e] = values[e].replace(digit[0], '')
+                    values[e] = values[e].replace(digit[1], '')
+            count += 1
+
+    return values
+
+def solve(grid):
+    """
+    Find the solution to a Sudoku grid.
+    Args:
+        grid(string): a string representing a sudoku grid.
+            Example: '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    Returns:
+        The dictionary representation of the final sudoku grid. False if no solution exists.
+    """
+    values = grid_values(grid)
+    values=search(values)
+    return values
+
+if __name__ == '__main__':
+    diag_sudoku_grid = '2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3'
+    display(solve(diag_sudoku_grid))
+
+    try:
+        from visualize import visualize_assignments
+        visualize_assignments(assignments)
+
+    except SystemExit:
+        pass
+    except:
+        print('We could not visualize your board due to a pygame issue. Not a problem! It is not a requirement.')
